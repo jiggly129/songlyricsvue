399e4e14ce77a9e28f5ff494f97c63195fb4ed5c
jack hoff <mono.hox@gmail.com>
2026-06-14 00:06:30 +0200

    af

--- api/playlist.js
+++ api/playlist.js
@@ -100,15 +100,17 @@ const allowedOrigins = [
 
 import YouTube from "youtube-sr";
 
-export default async function extractPlaylistSongs(req) {
-   const origin = req.headers.origin
+export default async function getPlaylist(req, res) {
+  const origin = req.headers.origin;
 
   if (allowedOrigins.includes(origin)) {
-    res.setHeader('Access-Control-Allow-Origin', origin)
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  } else {
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
   }
 
-  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end()
